3292c0deb4a0e0ed796fbc96e69c6174d2097c14
Diego Palacios <pm15016@ues.edu.sv>
2024-05-02 23:34:42 -0600

    reporte de balanza de comprobacion, en proceso nueva consulta

---
+++ src/reportes/consulta-balanza-de-comprobacion.sql
@@ -0,0 +1,71 @@
+with cte_balanza_comprobacion as (
+  select c.id, pd.folio_mayor, c.codigo, c.nombre, c.tipo_saldo, 
+  case 
+    when tipo_saldo = 'D' then debe - haber
+    when tipo_saldo = 'A' then haber - debe
+    else 0 
+  end
+  as saldo_inicial, 
+  row_number() over (PARTITION by c.id order by p.fecha || ' ' || p.hora asc, p.id asc) as row_number
+  from cuenta c 
+  inner join partida_detalle pd on pd.id_cuenta = c.id and pd.parcial = 0
+  inner join partida p on pd.id_partida = p.id
+  where c.eliminado = false 
+  and p.eliminado = false
+  and pd.eliminado = false
+  and p.id_ciclo = 21
+  and c.id_tipo_catalogo = 1
+  and (
+      null is null or c.id = null
+  )
+)
+select cbc.*, saldo_calculado.saldo_deudor, saldo_calculado.saldo_acreedor
+from cte_balanza_comprobacion cbc 
+inner join (
+	select 
+		case tipo_saldo
+			when 'D' then saldo_inicial
+			when 'A' then 0
+			else 0 end 
+		as debe, 
+		case tipo_saldo
+			when 'D' then 0
+			when 'A' then saldo_inicial
+			else 0 end 
+		as haber
+		from cuenta_balance cb 
+		inner join cuenta c on cb.id_cuenta = c.id
+		where 
+		c.eliminado = false
+		and cb.id_cuenta = $P{param_id_cuenta} 
+		and cb.id_ciclo_contable = $P{param_id_ciclo_tabla} 
+	UNION
+  select c.id, 
+  sum(
+      case
+        when tipo_saldo = 'D' then debe - haber
+	else 0
+    end
+  ) as saldo_deudor, 
+  sum(
+      case
+        when tipo_saldo = 'A' then haber - debe
+	else 0
+      end
+  ) as saldo_acreedor
+  from cuenta c 
+    inner join partida_detalle pd on pd.id_cuenta = c.id
+    inner join partida p on pd.id_partida = p.id
+    where c.eliminado = false 
+    and p.eliminado = false
+    and pd.eliminado = false
+    and p.id_ciclo = 21
+    and c.id_tipo_catalogo = 1
+	and p.id_tipo_partida <= 1
+	and (
+      null is null or c.id = null
+    )
+  group by c.id, c.codigo, c.nombre, c.tipo_saldo
+) as saldo_calculado on saldo_calculado.id = cbc.id
+where row_number = 1
+order by folio_mayor
