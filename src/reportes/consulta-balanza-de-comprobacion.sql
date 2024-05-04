2cd0db98fd043180531afe844992471454a33a49
Diego Palacios <pm15016@ues.edu.sv>
2024-05-04 11:29:30 -0600

    reporte balanza de comprobacion, en proceso nueva consulta, se planea integrar vistas

--- src/reportes/consulta-balanza-de-comprobacion.sql
+++ src/reportes/consulta-balanza-de-comprobacion.sql
@@ -1,3 +1,31 @@
+/*Vista para simplificar las consultas que recuperan los saldos de las cuentas*/
+
+create view vw_libro_diario as 
+select 
+	0 as id_partida,
+	cb.id_cuenta, 
+	0 as folio_mayor,
+	case tipo_saldo
+		when 'D' then saldo_inicial
+		when 'A' then 0
+		else 0 end 
+	as debe, 
+	case tipo_saldo
+		when 'D' then 0
+		when 'A' then saldo_inicial
+		else 0 end 
+	as haber, 
+	0 as parcial, 
+    0 as eliminado
+	from cuenta_balance cb 
+	inner join cuenta c on cb.id_cuenta = c.id
+UNION
+select 
+	pdi.id_partida, pdi.id_cuenta, pdi.folio_mayor, pdi.debe, pdi.haber, pdi.parcial, pdi.eliminado
+from partida_detalle pdi
+
+select * from vw_libro_diario;
+
 with cte_balanza_comprobacion as (
   select c.id, pd.folio_mayor, c.codigo, c.nombre, c.tipo_saldo, 
   case 
@@ -8,7 +36,7 @@ with cte_balanza_comprobacion as (
   as saldo_inicial, 
   row_number() over (PARTITION by c.id order by p.fecha || ' ' || p.hora asc, p.id asc) as row_number
   from cuenta c 
-  inner join partida_detalle pd on pd.id_cuenta = c.id and pd.parcial = 0
+  inner join vw_libro_diario pd on pd.id_cuenta = c.id and pd.parcial = 0
   inner join partida p on pd.id_partida = p.id
   where c.eliminado = false 
   and p.eliminado = false
@@ -22,24 +50,6 @@ with cte_balanza_comprobacion as (
 select cbc.*, saldo_calculado.saldo_deudor, saldo_calculado.saldo_acreedor
 from cte_balanza_comprobacion cbc 
 inner join (
-	select 
-		case tipo_saldo
-			when 'D' then saldo_inicial
-			when 'A' then 0
-			else 0 end 
-		as debe, 
-		case tipo_saldo
-			when 'D' then 0
-			when 'A' then saldo_inicial
-			else 0 end 
-		as haber
-		from cuenta_balance cb 
-		inner join cuenta c on cb.id_cuenta = c.id
-		where 
-		c.eliminado = false
-		and cb.id_cuenta = $P{param_id_cuenta} 
-		and cb.id_ciclo_contable = $P{param_id_ciclo_tabla} 
-	UNION
   select c.id, 
   sum(
       case
@@ -54,8 +64,9 @@ inner join (
       end
   ) as saldo_acreedor
   from cuenta c 
-    inner join partida_detalle pd on pd.id_cuenta = c.id
-    inner join partida p on pd.id_partida = p.id
+    left join partida p on pd.id_partida = p.id
+    inner join vw_libro_diario pd on pd.id_cuenta = c.id
+    
     where c.eliminado = false 
     and p.eliminado = false
     and pd.eliminado = false
